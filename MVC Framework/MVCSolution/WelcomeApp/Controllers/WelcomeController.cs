3947e3952295ada354d37e324fd18249f5c57013
raj <rajswabhav@outlook.com>
2018-01-12 12:24:42 +0530

    added

--- MVC Framework/MVCSolution/WelcomeApp/Controllers/WelcomeController.cs
+++ MVC Framework/MVCSolution/WelcomeApp/Controllers/WelcomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WelcomeApp.Models.ViewModel;
 
 namespace WelcomeApp.Controllers
 {
@@ -13,5 +14,26 @@ namespace WelcomeApp.Controllers
             return "<h1>Welcome to MVC</h1>";
         }
 
+        public string GetEmp(int? id)
+        {
+            return "Get Employee Details of "+id;
+        }
+
+        public ActionResult SayHello(string name)
+        {
+            return Content("Hello " + name);
+        }
+
+        public ActionResult Browse()
+        {
+            ViewBag.Message = "Hello Swabhav";
+            return View();
+        }
+
+        public ActionResult DisplayEmp()
+        {
+            DisplayEmpVM vm = new DisplayEmpVM() { Name="Raj",Salary=15000,DateOfHire="14-April-2017"};
+            return View(vm);
+        }
     }
 }
---
+++ MVC Framework/MVCSolution/WelcomeApp/Models/ViewModel/DisplayEmpVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WelcomeApp.Models.ViewModel
+{
+    public class DisplayEmpVM
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string DateOfHire{get;set;}
+    }
+}
